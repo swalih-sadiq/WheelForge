254ea76d82a7424c3a1891dd002e6a49aeaa1cfc
swalih <swalih.s.dev@gmail.com>
2026-01-28 17:42:21 +0530

    add address under profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ urlpatterns=[
     path('email-change/', views.request_email_change_view, name='request-email-change'),
     path('email-change/verify', views.verify_email_change_otp_view, name='verify-email-change-otp'),
 
-    path('addresses/', views.address_list_view, name='address-list'),
-    path('addresses/add/', views.add_address_view, name='add-address'),
-    path('addresses/edit/<int:address_id>/', views.edit_address_view, name='edit-address'),
-    path('addresses/delete/<int:address_id>/', views.delete_address_view, name='delete-address'),
+    path('pofile/addresses/', views.address_list_view, name='address-list'),
+    path('profile/addresses/add/', views.add_address_view, name='add-address'),
+    path('profile/addresses/edit/<int:address_id>/', views.edit_address_view, name='edit-address'),
+    path('profile/addresses/delete/<int:address_id>/', views.delete_address_view, name='delete-address'),
 ]
