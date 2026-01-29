6ac5168c62908b1dff4dd19e13c84bb86b756a0e
swalih <swalih.s.dev@gmail.com>
2026-01-29 13:48:58 +0530

    feat: admin user management with block and search

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('login/', views.admin_login_view, name='admin-login'),
     path('logout/', views.admin_logout_view, name='admin-logout'),
-    path('dashboard/',views.admin_dashboard_view, name='admin-dashboard')    
+    path('dashboard/',views.admin_dashboard_view, name='admin-dashboard'),
+
+    path('users/', views.admin_user_list_view, name='admin-user-list'),
+    path('users/block/<int:user_id>/', views.admin_block_user_view, name='admin-block-user'),
+    path('users/unblock/<int:user_id>/', views.admin_unblock_user_view, name='admin-unblock-user'),    
 ]
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.views.decorators.http import require_POST
 from django.contrib.auth import authenticate, login, logout
+from accounts.models import User
 from .decorators import admin_required
 
 # Create your views here.
@@ -30,4 +33,52 @@ def admin_dashboard_view(request):
 def admin_logout_view(request):
     logout(request)
     return redirect('admin-login')
+
+
+@admin_required
+def admin_user_list_view(request):
+    query =  request.GET.get('q', '').strip()
+
+    users = User.objects.all().order_by('-created_at')
+
+    if query:
+        users = users.filter(email__icontains=query)
+
+    paginator = paginator(users, 20)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'adminpanel/user_list.html', 
+                  {
+                      'page_obj': page_obj,
+                      'query': query,
+                  },
+                  )
+
+@admin_required 
+@require_POST
+def admin_block_user_view(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+
+    if user == request.user:
+        messages.error(request, 'You cannot block yourself.')
+        return redirect('admin-user-list')
+    
+    user.is_blocked = True 
+    user.save()
+
+    messages.success(request, f'{user.email} has been blocked.')
+    return redirect('admin-user-list')
+
+
+@admin_required
+@require_POST 
+def admin_unblock_user_view(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+
+    user.is_blocked = False 
+    user.save()
+
+    messages.success(request, f'{user.email} has been blocked.')
+    return redirect('admin-user-list')
 
